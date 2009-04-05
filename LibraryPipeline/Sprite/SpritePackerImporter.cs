9043d1882b463667548fb6dce2d587056edbc900
eric <eric@localhost>
2009-04-04 23:39:51 -0400

    Stubbed out sprite/texture packer.

---
+++ LibraryPipeline/Sprite/SpritePackerImporter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content.Pipeline;
+
+
+namespace LibraryPipeline.Sprite
+{
+    /// <summary>
+    /// Imports the packed sprite XML.
+    /// </summary>
+    [ContentImporter(".sprites", DisplayName = "Packed Sprites Importer")]
+    public class SpriteDescriptorImporter : ContentImporter<Dictionary<string,string[]>>
+    {
+    }
+}
