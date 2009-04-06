06957d80fb2f4638edc0ab121f49f5f1bbff9058
eric <eric@localhost>
2009-04-06 17:31:17 -0400

    Stubbed out a rough API for the texture packer. Probably needs more refinement.

--- LibraryPipeline/Sprite/TexturePacker.cs
+++ LibraryPipeline/Sprite/TexturePacker.cs
@@ -1,11 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
 
 namespace LibraryPipeline.Sprite
 {
+    public class PackedTexture
+    {
+        /// <summary>
+        /// The parent container texture.
+        /// </summary>
+        public BitmapContent Container { get; private set; }
+
+        /// <summary>
+        /// The original packed texture.
+        /// </summary>
+        public BitmapContent Texture { get; private set; }
+
+        /// <summary>
+        /// The position of the child texture inside its container.
+        /// </summary>
+        public Rectangle Bounds { get; private set; }
+    }
+
+    /// <summary>
+    /// Packs textures into one or more container textures.
+    /// </summary>
     public class TexturePacker
     {
+        /// <summary>
+        /// Creates a new texture packer.
+        /// </summary>
+        /// <param name="size">The width/height of the container texture.</param>
+        public TexturePacker(int size)
+        {
+            _size = size;
+        }
+
+        /// <summary>
+        /// Packs the specified textures.
+        /// </summary>
+        /// <param name="textures">The textures to pack.</param>
+        public List<PackedTexture> Pack(List<BitmapContent> textures)
+        {
+            return null;
+        }
+
+        private int _size;
     }
 }
