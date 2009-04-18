ce7fe74a20ec20e6fbbdaff906f9dac914e6c646
eric <eric@localhost>
2009-04-18 16:33:24 -0400

    Added GameTime and MathHelper extension methods.

---
+++ Library/Extensions/MathHelperExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+
+namespace Library.Extensions
+{
+    /// <summary>
+    /// Extensions to MathHelper.
+    /// </summary>
+    public static class MathHelperExtensions
+    {
+        /// <summary>
+        /// Returns abs(a - b) <= epsilon.
+        /// </summary>
+        /// <param name="a">The first number to compare.</param>
+        /// <param name="b">The second number to compare.</param>
+        /// <param name="epsilon">The epsilon tolerance.</param>
+        /// <returns>If a and b are within epsilon of each other.</returns>
+        public static bool EpsilonEquals(this MathHelper math, float a, float b, float epsilon)
+        {
+            return Math.Abs(a - b) <= epsilon;
+        }
+    }
+}
--- Library/Input/Vibration.cs
+++ Library/Input/Vibration.cs
@@ -7,43 +7,79 @@ using Library.Animation;
 
 namespace Library.Input
 {
-    public struct VibrationKeyFrame
-    {
-        public readonly Vector2 Amount;
-        public readonly float Duration;
-        public readonly Ease Ease;
-
-        public VibrationKeyFrame(Vector2 amount, float duration) : this(amount, duration, Easing.Uniform)
-        {
-        }
-
-        public VibrationKeyFrame(Vector2 amount, float duration, Ease easing)
-        {
-            Amount = amount;
-            Duration = duration;
-            Ease = easing;
-        }
-    }
-
     /// <summary>
     /// A vibration animation.
     /// </summary>
-    public class Vibration : IAnimation
+    public class VibrationAnimation : IAnimation
     {
+        public struct VibrationKeyFrame
+        {
+            public readonly Vector2 Amount;
+            public readonly float Duration;
+            public readonly Ease Ease;
+
+            public VibrationKeyFrame(Vector2 amount)
+                : this(amount, 0f)
+            {
+            }
+
+            public VibrationKeyFrame(Vector2 amount, float duration)
+                : this(amount, duration, Easing.Uniform)
+            {
+            }
+
+            public VibrationKeyFrame(Vector2 amount, float duration, Ease easing)
+            {
+                Amount = amount;
+                Duration = duration;
+                Ease = easing;
+            }
+        }
+
         /// <summary>
         /// Creates a constant vibration.
         /// </summary>
-        /// <param name="amount">The strength (x: low frequency, y: high frequency) in [0, 1].</param>
-        /// <param name="duration">The duration in seconds.</param>
-        public static Vibration Constant(Vector2 amount, float duration)
+        /// <param name="leftMotor">The speed of the left, low-frequency motor in [0, 1].</param>
+        /// <param name="rightMotor">The speed of the right, high-frequency motor in [0, 1].</param>
+        /// <param name="duration">The duration, in seconds, to vibrate for.</param>
+        public static VibrationAnimation Constant(float leftMotor, float rightMotor, float duration)
         {
-            return new Vibration(new VibrationKeyFrame(amount, duration), new VibrationKeyFrame(amount, duration));
+            Vector2 amount = new Vector2(leftMotor, rightMotor);
+            return new VibrationAnimation(
+                new VibrationKeyFrame(amount, duration),
+                new VibrationKeyFrame(amount));
+        }
+
+        /// <summary>
+        /// Creates a vibration that fades in from nothing to a final amount.
+        /// </summary>
+        /// <param name="amount">The final amount of vibration.</param>
+        /// <param name="duration">The time, in seconds, to fade in.</param>
+        /// <returns>The vibration animation.</returns>
+        public static VibrationAnimation FadeIn(float leftMotor, float rightMotor, float duration, Ease easing)
+        {
+            return new VibrationAnimation(
+                new VibrationKeyFrame(Vector2.Zero, duration, easing),
+                new VibrationKeyFrame(new Vector2(leftMotor, rightMotor)));
+        }
+
+        /// <summary>
+        /// Creates a vibration that starts at some amount and fades to nothing.
+        /// </summary>
+        /// <param name="amount">The final amount of vibration.</param>
+        /// <param name="duration"></param>
+        /// <returns></returns>
+        public static VibrationAnimation FadeOut(float leftMotor, float rightMotor, float duration, Ease easing)
+        {
+            return new VibrationAnimation(
+                new VibrationKeyFrame(new Vector2(leftMotor, rightMotor), duration, easing),
+                new VibrationKeyFrame(Vector2.Zero));
         }
 
         /// <summary>
         /// Creates a new vibration animation.
         /// </summary>
-        public Vibration(params VibrationKeyFrame[] frames)
+        public VibrationAnimation(params VibrationKeyFrame[] frames)
         {
             if (frames.Length < 2)
             {
