16656b80299216cc4159a2210f2f2b86e54f1415
eric <eric@localhost>
2009-04-25 15:42:25 -0400

    Finished the texture packer.

--- LibraryPipeline/Sprite/PackedImageSpritesProcessor.cs
+++ LibraryPipeline/Sprite/PackedImageSpritesProcessor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -7,6 +8,9 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Content.Pipeline;
 using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
+using Microsoft.Xna.Framework.Graphics;
+
+using LibraryPipeline.Extensions;
 
 namespace LibraryPipeline.Sprite
 {
@@ -16,32 +20,128 @@ namespace LibraryPipeline.Sprite
     [ContentProcessor(DisplayName = "Packed Image Sprite Processor")]
     public class PackedImageSpritesProcessor : ContentProcessor<PackedImageSpritesContent, DummyObject>
     {
+        /// <summary>
+        /// The possible sizes of the container texture.
+        /// </summary>
+        public enum ContainerSizes
+        {
+            S256 = 256,
+            S512 = 512,
+            S1024 = 1024,
+            S2048 = 2048,
+            S4096 = 4096
+        }
+
+        /// <summary>
+        /// The width and height, in texels, of the container texture.
+        /// </summary>
+        [DisplayName("Container Texture Size")]
+        [Description("The width and height, in texels, of the container texture.")]
+        [DefaultValue(ContainerSizes.S4096)]
+        public ContainerSizes ContainerSize { get; set; }
+
+        public PackedImageSpritesProcessor()
+        {
+            ContainerSize = ContainerSizes.S4096;
+        }
+
         public override DummyObject Process(PackedImageSpritesContent input, ContentProcessorContext context)
         {
-            TexturePacker packer = new TexturePacker(0);
+            TexturePacker packer = new TexturePacker((int)ContainerSize);
+            List<Texture2DContent> textures = new List<Texture2DContent>();
+
             foreach (var group in input.Groups)
             {
-                List<Texture2DContent> textures = new List<Texture2DContent>();
+                textures.Clear();
 
-                foreach (var file in group.Filenames)
+                // load the textures to be packed
+                foreach (var fileName in group.Filenames)
                 {
-                    string source = Path.GetDirectoryName(input.Identity.SourceFilename) + @"\" + file;
-                    textures.Add(context.BuildAndLoadAsset<Texture2DContent, Texture2DContent>(new ExternalReference<Texture2DContent>(source), null));
-                    context.AddDependency(source);
+                    string textureFile = Path.GetDirectoryName(input.Identity.SourceFilename) + @"\" + fileName;
+                    context.AddDependency(textureFile);
+
+                    ExternalReference<Texture2DContent> textureReference = new ExternalReference<Texture2DContent>(textureFile);
+                    Texture2DContent texture = context.BuildAndLoadAsset<Texture2DContent, Texture2DContent>(textureReference, null);
+                    texture.Name = Path.GetFileNameWithoutExtension(fileName);
+                    textures.Add(texture);
                 }
 
-                List<PackedTexture> packed = packer.Pack(textures);
-
-                /*foreach (var pack in packed)
+                // pack the textures
+                int containerTextures = 0;
+                while (textures.Count > 0 && containerTextures < MaximumContainersPerGroup)
                 {
-                    ImageSpriteStub stub = new ImageSpriteStub(new ExternalReference<Texture2DContent>(""), pack.Bounds);
-                    // write the stub
-                }*/
-                // write out container textures
-                // write out individual sprites
+                    packer.Pack(textures);
+                    var packedTextures = packer.GetContents();
+
+                    Texture2DContent containerTexture = CreateContainerTexture(packedTextures);
+                    ExternalReference<Texture2DContent> container = context.WriteAsset(containerTexture, group.Name + "Texture" + containerTextures);
+
+                    foreach (var pack in packedTextures)
+                    {
+                        // undo the padding
+                        Rectangle texturePosition = pack.Value;
+                        texturePosition.X += 1;
+                        texturePosition.Y += 1;
+                        texturePosition.Width -= 2;
+                        texturePosition.Height -= 2;
+
+                        // write the stub
+                        ImageSpriteStub stub = new ImageSpriteStub(container, texturePosition);
+                        context.WriteAsset(stub, pack.Key.Name);
+
+                        context.Logger.LogMessage("Packed {0} at {1}", pack.Key.Name, texturePosition);
+                    }
+
+                    // repeat with the textures that did not fit
+                    textures = packer.GetOverflow();
+                    containerTextures++;
+                }
+
+                if (containerTextures > MaximumContainersPerGroup)
+                {
+                    context.Logger.LogImportantMessage(
+                        "Too many textures to pack in group {0}; {1} left over.", group.Name, textures.Count);
+                }
             }
 
             return new DummyObject();
         }
+
+        private Texture2DContent CreateContainerTexture(List<KeyValuePair<Texture2DContent, Rectangle>> textures)
+        {
+            BitmapContent output = new PixelBitmapContent<Color>((int)ContainerSize, (int)ContainerSize);
+            foreach (var textureInfo in textures)
+            {
+                BitmapContent texture = textureInfo.Key.Mipmaps[0];
+                int w = texture.Width;
+                int h = texture.Height;
+
+                Rectangle texturePosition = textureInfo.Value;
+                int x = texturePosition.X;
+                int y = texturePosition.Y;
+
+                // Copy the main sprite data to the output sheet
+                BitmapContent.Copy(texture, new Rectangle(0, 0, w, h), output, new Rectangle(x + 1, y + 1, w, h));
+
+                // Copy a border strip from each edge of the sprite, creating
+                // a one pixel padding area to avoid filtering problems if the
+                // sprite is scaled or rotated
+                BitmapContent.Copy(texture, new Rectangle(0, 0, 1, h), output, new Rectangle(x, y + 1, 1, h));
+                BitmapContent.Copy(texture, new Rectangle(w - 1, 0, 1, h), output, new Rectangle(x + w + 1, y + 1, 1, h));
+                BitmapContent.Copy(texture, new Rectangle(0, 0, w, 1), output, new Rectangle(x + 1, y, w, 1));
+                BitmapContent.Copy(texture, new Rectangle(0, h - 1, w, 1), output, new Rectangle(x + 1, y + h + 1, w, 1));
+
+                // Copy a single pixel from each corner of the sprite, filling in the corners of the one pixel padding area
+                BitmapContent.Copy(texture, new Rectangle(0, 0, 1, 1), output, new Rectangle(x, y, 1, 1));
+                BitmapContent.Copy(texture, new Rectangle(w - 1, 0, 1, 1), output, new Rectangle(x + w + 1, y, 1, 1));
+                BitmapContent.Copy(texture, new Rectangle(0, h - 1, 1, 1), output, new Rectangle(x, y + h + 1, 1, 1));
+                BitmapContent.Copy(texture, new Rectangle(w - 1, h - 1, 1, 1), output, new Rectangle(x + w + 1, y + h + 1, 1, 1));
+            }
+            Texture2DContent tex2d = new Texture2DContent();
+            tex2d.Mipmaps = new MipmapChain(output);
+            return tex2d;
+        }
+
+        private const int MaximumContainersPerGroup = 20;
     }
 }
--- LibraryPipeline/Sprite/TexturePacker.cs
+++ LibraryPipeline/Sprite/TexturePacker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 using Microsoft.Xna.Framework;
@@ -6,24 +7,6 @@ using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
 
 namespace LibraryPipeline.Sprite
 {
-    public class PackedTexture
-    {
-        /// <summary>
-        /// The parent container texture.
-        /// </summary>
-        public Texture2DContent Container { get; private set; }
-
-        /// <summary>
-        /// The original packed texture.
-        /// </summary>
-        public Texture2DContent Texture { get; private set; }
-
-        /// <summary>
-        /// The position of the child texture inside its container.
-        /// </summary>
-        public Rectangle Bounds { get; private set; }
-    }
-
     /// <summary>
     /// Packs textures into one or more container textures.
     /// </summary>
@@ -36,17 +19,139 @@ namespace LibraryPipeline.Sprite
         public TexturePacker(int size)
         {
             _size = size;
+            _packed = new List<KeyValuePair<Texture2DContent, Rectangle>>();
+            _overflow = new List<Texture2DContent>();
         }
 
         /// <summary>
         /// Packs the specified textures.
         /// </summary>
         /// <param name="textures">The textures to pack.</param>
-        public List<PackedTexture> Pack(List<Texture2DContent> textures)
+        /// <returns>True if every texture was successfully packed; otherwise, false.</returns>
+        public bool Pack(List<Texture2DContent> textures)
         {
-            return null;
+            _packed.Clear();
+            _overflow.Clear();
+
+            // sort the textures by area
+            textures.Sort((a, b) =>
+                (a.Mipmaps[0].Width * a.Mipmaps[0].Height).CompareTo(
+                 b.Mipmaps[0].Width * b.Mipmaps[0].Height));
+
+            // pack as many textures into the container as will fit
+            Node root = new Node(new Rectangle(0, 0, _size, _size));
+
+            int texidx;
+            for (texidx = 0; texidx < textures.Count; texidx++)
+            {
+                Texture2DContent texture = textures[texidx];
+                Rectangle size = new Rectangle(0, 0, texture.Mipmaps[0].Width + 2, texture.Mipmaps[0].Height + 2); // pad the size
+                Rectangle? position = root.Insert(size);
+                if (position != null)
+                {
+                    _packed.Add(new KeyValuePair<Texture2DContent, Rectangle>(texture, position.Value));
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            // track the overflow items
+            for (; texidx < textures.Count; texidx++)
+            {
+                _overflow.Add(textures[texidx]);
+            }
+
+            // indicate if there was overflow
+            return _overflow.Count == 0;
+        }
+
+        /// <summary>
+        /// Returns the textures packed inside this container.
+        /// </summary>
+        public List<KeyValuePair<Texture2DContent, Rectangle>> GetContents()
+        {
+            return _packed;
+        }
+
+        /// <summary>
+        /// Returns the textures that were added but could not be packed in the container.
+        /// </summary>
+        public List<Texture2DContent> GetOverflow()
+        {
+            return _overflow;
+        }
+
+        /// <summary>
+        /// A node in a storage tree.
+        /// </summary>
+        private class Node
+        {
+            public Node(Rectangle space)
+            {
+                _box = space;
+                _child = null;
+                _occupied = false;
+            }
+
+            /// <summary>
+            /// Inserts a rectangle into the container.
+            /// </summary>
+            /// <param name="insert">The rectangle to insert.</param>
+            /// <returns>The position of the rectangle, or null if it did not fit.</returns>
+            public Rectangle? Insert(Rectangle insert)
+            {
+                if (_child != null)
+                {
+                    // if we're a parent try inserting into our children
+                    Rectangle? box = _child[0].Insert(insert);
+                    if (box != null)
+                    {
+                        return box;
+                    }
+                    else
+                    {
+                        return _child[1].Insert(insert);
+                    }
+                }
+                else
+                {
+                    // otherwise create children in which to store the box
+                    if (_occupied || insert.Width > _box.Width || insert.Height > _box.Height)
+                    {
+                        return null;
+                    }
+                    else if (insert.Width == _box.Width && insert.Height == _box.Height)
+                    {
+                        _occupied = true;
+                        return _box;
+                    }
+
+                    _child = new Node[2];
+
+                    if ((_box.Width - insert.Width) > (_box.Height - insert.Height)) // delta width > delta height
+                    {
+                        _child[0] = new Node(new Rectangle(_box.X, _box.Y, insert.Width, _box.Height));
+                        _child[1] = new Node(new Rectangle(_box.X + insert.Width, _box.Y, _box.Width - insert.Width, _box.Height));
+                    }
+                    else
+                    {
+                        _child[0] = new Node(new Rectangle(_box.X, _box.Y, insert.Width, insert.Height));
+                        _child[1] = new Node(new Rectangle(_box.X, _box.Y + insert.Height, _box.Width, _box.Height - insert.Height));
+                    }
+
+                    return _child[0].Insert(insert);
+                }
+            }
+
+            private Rectangle _box; /// space occupied by this node
+            private Node[] _child;  /// children of this node
+            private bool _occupied; /// if this node contains a box
         }
 
         private int _size;
+        private List<KeyValuePair<Texture2DContent, Rectangle>> _packed;
+        private List<Texture2DContent> _overflow;
     }
 }
